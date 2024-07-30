3ed2bbc1f1889cb645b6b467123586e3a0e39b7c
Gauatm-raj <gautamraj7569@gmail.com>
2024-07-30 23:19:42 +0530

    DONE AGAIN

--- src/main/java/com/hotelservice/Book/Hotel/Config/WebConfig.java
+++ src/main/java/com/hotelservice/Book/Hotel/Config/WebConfig.java
@@ -15,6 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://127.0.0.1:5173")
+                        .allowedOrigins("https://hotel-frontend-eight.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
